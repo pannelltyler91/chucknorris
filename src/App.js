34af293f02d37607931f1827258715aa966ed04a
unknown <you@example.com>
2021-09-25 13:32:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import {React,Component} from 'react';
+
+class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      joke:""
+    }
+  }
+
+  _handleclick = () =>{
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(res => res.json())
+    .then(data => {
+      
+      this.setState({
+        joke:data.value
+      })
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Chuck Norris Jokes</h1>
+        </header>
+          <div className='jokeContainer'>
+            {this.state.joke}
+          </div>
+          <button onClick={this._handleclick}>Generate Joke</button>
+      </div>
+    );
+  }
+  }
 
 export default App;
